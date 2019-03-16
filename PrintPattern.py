96587b45f1dff868ff7bfba47a02aa7932e3247e
Rufaidah <rufaidah.chan@gmail.com>
2019-03-16 10:54:26 +0700

    edit print pattern

--- PrintPattern.py
+++ PrintPattern.py
@@ -1,23 +1,19 @@
-side = int(input("MAsukkan sisi persegi : "))
+side = int(input("Masukkan sisi persegi : "))
 
 def drawImage(side):
-    if side % 2 == 0:
-        return False
-    else:
-        # row
-        for i in range(side):
-            # column
-            for j in range(side):
-                if (i == (side - 1) / 2) or (j == (side - 1) / 2) \
-                        or (i | j == 0) or (i == 0 and j == side - 1) \
-                        or (i == side - 1 and j == 0) or (i == side - 1 and j == side - 1):
-                    print("*", end='')
-                else:
-                    print("=", end='')
-            print()
-        return True
+    # row
+    for i in range(side):
+        # column
+        for j in range(side):
+            if (i == (side - 1) / 2) or (j == (side - 1) / 2) \
+                    or (i | j == 0) or (i == 0 and j == side - 1) \
+                    or (i == side - 1 and j == 0) or (i == side - 1 and j == side - 1):
+                print("*", end='')
+            else:
+                print("=", end='')
+        print()
 
-if drawImage(side) == True:
-    print()
-else:
+if side % 2 == 0:
     print("Panjang sisi harus ganjil")
+else:
+    drawImage(side)
